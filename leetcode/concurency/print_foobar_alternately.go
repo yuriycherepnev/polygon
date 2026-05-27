83414c09bd386aa5e2cb5fd97a209a78839a9c42
yuriy <yuriy.cherepnev@gmail.com>
2026-05-27 17:36:59 +0300

    5555

--- leetcode/concurency/print_foobar_alternately.go
+++ leetcode/concurency/print_foobar_alternately.go
@@ -7,6 +7,138 @@ import (
 
 type FooBar struct {
 	n   int
+	chn chan struct{}
+}
+
+func NewFooBar(n int) *FooBar {
+	f := &FooBar{
+		n:   n,
+		chn: make(chan struct{}),
+	}
+	return f
+}
+
+func (fb *FooBar) Foo(printFoo func()) {
+	for i := 0; i < fb.n; i++ {
+		// printFoo() outputs "foo". Do not change or remove this line.
+		printFoo()
+		fb.chn <- struct{}{}
+		<-fb.chn
+	}
+}
+
+func (fb *FooBar) Bar(printBar func()) {
+	for i := 0; i < fb.n; i++ {
+		<-fb.chn
+		// printBar() outputs "bar". Do not change or remove this line.
+		printBar()
+		fb.chn <- struct{}{}
+	}
+}
+
+func main() {
+	foo := NewFooBar(5)
+	var wg sync.WaitGroup
+
+	printFoo := func() { fmt.Print("foo") }
+	printBar := func() { fmt.Print("bar") }
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		foo.Foo(printFoo)
+	}()
+
+	go func() {
+		defer wg.Done()
+		foo.Bar(printBar)
+	}()
+
+	wg.Wait()
+
+	fmt.Println()
+}
+
+// FooBar решение через атомики
+/*
+Функция runtime.Gosched() в языке Go принудительно возвращает текущую горутину
+в глобальную очередь и освобождает процессор.
+*/
+/*
+type FooBar struct {
+	n     int
+	isBar atomic.Bool
+}
+
+func NewFooBar(n int) *FooBar {
+	return &FooBar{
+		n:     n,
+		isBar: atomic.Bool{},
+	}
+}
+
+func (fb *FooBar) Foo(printFoo func()) {
+	for i := 0; i < fb.n; i++ {
+		for fb.isBar.Load() {
+			runtime.Gosched()
+		}
+		// printFoo() outputs "foo". Do not change or remove this line.
+		printFoo()
+		fb.isBar.Store(true)
+	}
+}
+
+func (fb *FooBar) Bar(printBar func()) {
+	for i := 0; i < fb.n; i++ {
+		for !fb.isBar.Load() {
+			runtime.Gosched()
+		}
+		// printBar() outputs "bar". Do not change or remove this line.
+		printBar()
+		fb.isBar.Store(false)
+	}
+}
+*/
+
+// решение в помощью channels
+/*
+type FooBar struct {
+	n   int
+	chn chan int
+}
+
+func NewFooBar(n int) *FooBar {
+	f := &FooBar{
+		n:   n,
+		chn: make(chan int),
+	}
+	return f
+}
+
+func (fb *FooBar) Foo(printFoo func()) {
+	for i := 0; i < fb.n; i++ {
+		// printFoo() outputs "foo". Do not change or remove this line.
+		printFoo()
+		fb.chn <- 0
+		<-fb.chn
+	}
+}
+
+func (fb *FooBar) Bar(printBar func()) {
+	for i := 0; i < fb.n; i++ {
+		<-fb.chn
+		// printBar() outputs "bar". Do not change or remove this line.
+		printBar()
+		fb.chn <- 0
+	}
+}
+*/
+
+// решение в помощью wg
+/*
+type FooBar struct {
+	n   int
 	foo sync.WaitGroup
 	bar sync.WaitGroup
 }
@@ -38,27 +170,4 @@ func (fb *FooBar) Bar(printBar func()) {
 		}
 	}
 }
-
-func main() {
-	foo := NewFooBar(1)
-	var wg sync.WaitGroup
-
-	printFoo := func() { fmt.Print("foo") }
-	printBar := func() { fmt.Print("bar") }
-
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		foo.Foo(printFoo)
-	}()
-
-	go func() {
-		defer wg.Done()
-		foo.Bar(printBar)
-	}()
-
-	wg.Wait()
-
-	fmt.Println()
-}
+*/
