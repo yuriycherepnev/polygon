5ceec7677187d002cbf30c43e9ccf8596f07ec97
yuriy <yuriy.cherepnev@gmail.com>
2026-06-08 16:24:59 +0300

    5555

--- leetcode/concurency/h2o.go
+++ leetcode/concurency/h2o.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	water := "OHOOHOHHHHOHHOHHHH"
+	water := "OHHOOOHOHHHHHHH"
 	h2o := NewH2O()
 	var wg sync.WaitGroup
 	for _, ch := range water {
@@ -26,37 +26,52 @@ func main() {
 		}
 	}
 	wg.Wait()
-
 	fmt.Println()
 }
 
 type H2O struct {
 	hCh     chan struct{}
 	oCh     chan struct{}
+	mu      sync.Mutex
+	cond    *sync.Cond
 	counter atomic.Int32
 }
 
 func NewH2O() *H2O {
 	h := &H2O{
-		hCh: make(chan struct{}, 2),
-		oCh: make(chan struct{}, 2),
+		hCh:     make(chan struct{}, 2),
+		oCh:     make(chan struct{}, 2),
+		counter: atomic.Int32{},
 	}
+	h.counter.Store(2)
+	h.cond = sync.NewCond(&h.mu)
 	h.oCh <- struct{}{}
 	return h
 }
 
 func (h *H2O) Hydrogen(releaseHydrogen func()) {
 	<-h.hCh
-	// releaseHydrogen() outputs "H". Do not change or remove this line.
+	newVal := h.counter.Add(-1)
+	if newVal == 0 {
+		h.mu.Lock()
+		h.cond.Signal()
+		h.mu.Unlock()
+	}
 	releaseHydrogen()
 }
 
 func (h *H2O) Oxygen(releaseOxygen func()) {
 	<-h.oCh
-	// releaseOxygen() outputs "H". Do not change or remove this line.
 	releaseOxygen()
 	h.hCh <- struct{}{}
 	h.hCh <- struct{}{}
+
+	h.mu.Lock()
+	for h.counter.Load() > 0 {
+		h.cond.Wait()
+	}
+	h.counter.Store(2)
+	h.mu.Unlock()
 
 	h.oCh <- struct{}{}
 }
