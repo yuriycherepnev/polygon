2d748b11512a2e49fcad1af31891b25e7c7fac5c
yuriy <yuriy.cherepnev@gmail.com>
2026-06-25 12:18:07 +0300

    111

--- concurency/cancel_work/channel.go
+++ concurency/cancel_work/channel.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func randomTimeWorkCh() {
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Second)
+}
+
+func predictableTimeWorkCh() error {
+	ch := make(chan struct{})
+
+	go func() {
+		randomTimeWorkCh()
+		close(ch)
+	}()
+
+	select {
+	case <-ch:
+		return nil
+	case <-time.After(3 * time.Second):
+		return errors.New("time is expired")
+	}
+}
+
+func main() {
+	err := predictableTimeWorkCh()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("goroutine worked correctly")
+	}
+}
