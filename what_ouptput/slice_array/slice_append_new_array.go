50ca567dd2a9c0cd53a012ff1bdb886f095ccc2b
yuriy <yuriy.cherepnev@gmail.com>
2026-07-18 13:43:34 +0300

    111

---
+++ what_ouptput/slice_array/slice_append_new_array.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3} // len 3 cap 3
+
+	println("cap(a) =", cap(a))
+
+	b := append(a, 4)
+	c := append(a, 5)
+	c[1] = 0
+
+	fmt.Println("a =", a)
+	fmt.Println("b =", b)
+	fmt.Println("c =", c)
+}
