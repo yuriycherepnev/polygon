b32149dc5ee90a4235b8fdf38a9d3c7928db09d6
yuriy <yuriy.cherepnev@gmail.com>
2026-07-22 12:05:58 +0300

    111

--- what_ouptput/slice_array/slice_append_7.go
+++ what_ouptput/slice_array/slice_append_7.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	s := make([]int, 0, 5) // [], 0 0 0 0 0, len 0 cap 5
-	s = append(s, 1, 2, 3) //[1 2 3], 0 0, len 3 cap 5
+	s := make([]int, 0, 5) // len=0, cap=5
+	s = append(s, 1, 2, 3) // len=3, cap=5, s = [1,2,3],0,0
 
-	subSlice := s[1:3] // [2 3] 0 0, len 2 cap 4
+	subSlice := s[1:3] // subSlice=[2,3],0,0; len=2, cap=4, based on s
 
-	subSlice[0] = 99               // [99 3] 0 0, len 2 cap 4
-	subSlice = append(subSlice, 4) // [99 3 4] 0 , len 3 cap 4
+	subSlice[0] = 99               // subSlice=[99,3],0,0; s=[1,99,3],0,0
+	subSlice = append(subSlice, 4) // len=3, cap=4, based on s, subSlice=[99,3,4],0; s=[1,99,3],4,0
 
-	s = append(s, 5, 6, 7) // [1 99 3 5 6 7] 0 0 0 0, len 6 cap 10, здесь создан новый массив
+	s = append(s, 5, 6, 7) // len=6, cap=10, new arr, s = [1,99,3, 5, 6, 7]
 
-	subSlice[1] = 100 // [99 100 4] 0
+	subSlice[1] = 100 // subSlice = [99, 100, 4],0
 
-	fmt.Println("s:", s)               // [1 99 3 5 6 7] 0 0 0 0, len 6 cap 10
-	fmt.Println("subSlice:", subSlice) // 0 [99 100 4] 0, len 3 cap 4
+	fmt.Println("s:", s)
+	fmt.Println("subSlice:", subSlice)
 }
