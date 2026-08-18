7c9ee2efa32371c91abe68671d1eea6cd9603744
yuriy <yuriy.cherepnev@gmail.com>
2026-08-18 14:51:01 +0300

    111

--- leetcode/anagram_str.go
+++ leetcode/anagram_str.go
@@ -1,1 +1,30 @@
+// Определить, являются ли две строки анаграммами. Строка А является анаграммой строки Б,
+// если можно переставить местами символы в строке А и получить строку Б.
+// Input: a="лапоть", b="пальто"
+// Output: true
+
 package main
+
+import "fmt"
+
+func main() {
+	singleNums := anagramStr("пальто", "лапоть")
+	fmt.Println(singleNums)
+}
+
+func anagramStr(strOne string, strTwo string) bool {
+	mapString := make(map[int32]int)
+
+	for _, value := range strOne {
+		mapString[value]++
+	}
+	for _, value := range strTwo {
+		mapString[value]--
+	}
+	for _, value := range mapString {
+		if value > 0 {
+			return false
+		}
+	}
+	return true
+}
