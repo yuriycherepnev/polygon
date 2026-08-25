695b0ec3348b6dbad69fb130e2ebc21badf13157
yuriy <yuriy.cherepnev@gmail.com>
2026-08-25 16:03:03 +0300

    111

--- concurency/cancel_work/template.go
+++ concurency/cancel_work/template.go
@@ -5,8 +5,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"math/rand"
 	"time"
 )
@@ -15,26 +13,6 @@ func randomTimeWork() {
 	time.Sleep(time.Duration(rand.Intn(100)) * time.Second)
 }
 
-func predictableTimeWork(ctx context.Context) error {
-	go func() {
-		randomTimeWork()
-	}()
-
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Done")
-			return ctx.Err()
-		default:
-			fmt.Println("Working...")
-			time.Sleep(500 * time.Millisecond)
-		}
-	}
-}
-
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-	err := predictableTimeWork(ctx)
-	fmt.Println(err)
+
 }
