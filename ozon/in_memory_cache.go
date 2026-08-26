5fe7d10532536a53c19ce3e048719112902fcd70
yuriy <yuriy.cherepnev@gmail.com>
2026-08-26 20:50:09 +0300

    111

--- ozon/in_memory_cache.go
+++ ozon/in_memory_cache.go
@@ -2,30 +2,30 @@ package main
 
 import "sync"
 
-type MCache interface {
+type Cache interface {
 	Set(k, v string)
 	Get(k string) (v string, ok bool)
 }
 
-type mCache struct {
+type cache struct {
 	mu   sync.RWMutex
 	data map[string]string
 }
 
-func NewMCache() MCache {
-	return &mCache{
+func NewMCache() Cache {
+	return &cache{
 		data: make(map[string]string),
 	}
 }
 
-func (c *mCache) Set(k, v string) {
+func (c *cache) Set(k, v string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	c.data[k] = v
 }
 
-func (c *mCache) Get(k string) (string, bool) {
+func (c *cache) Get(k string) (string, bool) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
@@ -34,6 +34,6 @@ func (c *mCache) Get(k string) (string, bool) {
 }
 
 func main() {
-	memCache := NewCache()
-	memCache.Set("123", "123")
+	mCache := NewMCache()
+	mCache.Set("123", "123")
 }
